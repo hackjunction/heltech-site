cec7424bdb7fbd12b7468e57f2a78ae6447a29b7
Visa Pollari <visapollari@Visas-MacBook-Pro.local>
2020-06-29 21:44:56 +0300

    Tweak api

--- frontend/app/src/services/contactrequests.js
+++ frontend/app/src/services/contactrequests.js
@@ -1,11 +1,11 @@
-import axios from "./axios";
+import axios from './axios';
 
-const URL = "https://cms.www.heltech.org/api/contactrequests";
+const URL = 'https://cms.www.heltech.org/api/contactrequests';
 
 const ContactRequestService = {
-  create: formData => {
-    return axios.post(URL, formData);
-  }
+    create: (formData) => {
+        return axios.post(URL, formData);
+    },
 };
 
 export default ContactRequestService;
